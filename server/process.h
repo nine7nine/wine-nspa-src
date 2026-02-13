1f70eadb83b961785c0056aa380622a12857e21c
Rémi Bernon <rbernon@codeweavers.com>
2026-02-13 23:23:45 +0100

    ntdll: Pass thread security descriptor to new_process request.

--- server/process.h
+++ server/process.h
@@ -55,6 +55,7 @@ struct process
     int                  exit_code;       /* process exit code */
     int                  running_threads; /* number of threads running in this process */
     unsigned int         thread_flags;    /* first thread flags */
+    struct security_descriptor *thread_sd; /* first thread security descriptor */
     timeout_t            start_time;      /* absolute time at process start */
     timeout_t            end_time;        /* absolute time at process end */
     affinity_t           affinity;        /* process affinity mask */
